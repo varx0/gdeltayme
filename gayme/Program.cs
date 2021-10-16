22f63348c09e938c34ee1836f2825f6dd6a469f3
goblinlordkingofelves420@gmail.com <goblinlordkingofelves420@gmail.com>
2021-10-15 20:30:07 -0500

    A blank window

--- gayme/Program.cs
+++ gayme/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Mathematics;
 
 namespace gayme
 {
     public class Program
-    {
+    {   
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            GameWindowSettings GWS = GameWindowSettings.Default;
+            NativeWindowSettings NWS = NativeWindowSettings.Default;
+
+            GWS.IsMultiThreaded = false;
+            GWS.RenderFrequency = 60;
+            GWS.UpdateFrequency = 60;
+
+            NWS.APIVersion = Version.Parse(input: "4.1.0");
+            NWS.Size = new Vector2i(x: 1280, y: 720);
+            NWS.Title = "Fuck you Jeff Bezos!";
+
+            GameWindow Window = new GameWindow(GWS, NWS);
+
+            Window.Run();
         }
     }
 }
